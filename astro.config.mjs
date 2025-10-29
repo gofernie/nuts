6473e9b9b8a2edaa751c527c67050862491f51c8
Chris Crump <gofernie@users.noreply.github.com>
2025-10-28 20:12:53 -0600

    Snapshot: make repo match Netlify uploaded deploy (690048d…)

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://fernie.realestate',
-  redirects: { '/todo': '/todo/page/1' },
-  server: { host: true, port: 4321, https: false },
-  vite: { server: { host: true, port: 4321, https: false } },
-});
